dc9e08be127253d581f2b946cf15d1626d0da898
agapoli89 <agapoli@gmail.com>
2020-11-15 14:14:32 +0100

    fetch breed by id and show image on the page

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
+const baseApiUrl = "https://api.thedogapi.com/v1/";
+const apiKey = {headers: {
+    'x-api-key': '230bebd6-4583-47f9-ad67-28d803c49adc',
+}};
+let picture = document.querySelector('.section__image');
+
 const getBreedsNames = async () => {
 
-    const apiKey = '230bebd6-4583-47f9-ad67-28d803c49adc';
-
     try {
-        const response = await fetch("https://api.thedogapi.com/v1/breeds", {headers: {
-            'x-api-key': apiKey,
-        }});
+        const response = await fetch(baseApiUrl + "breeds", apiKey);
         
         if (response.ok) {
             const data = await response.json();
-            console.log(data);
             chooseBreed(data); 
         } else {
             throw new Error(`Http error: ${response.status}`);
         }
     } catch(error) {
         console.log(error);
-        alert('Ups.. coś poszło nie tak. Spróbuj jeszcze raz :)');
-    }
-      
+        alert(`Ups.. it's something wrong. Try again :)`);
+    }  
 }
 
 const chooseBreed = (breeds) => {
@@ -33,11 +33,36 @@ const chooseBreed = (breeds) => {
     breedsOptions.forEach(breedsOption => {
         select.appendChild(breedsOption);
     });
-    select.addEventListener('change', showDog);
+    select.addEventListener('change', getDog);
 }
 
-const showDog = (e) => {
-    console.log(e.target.value);
+const getDog = async (e) => {
+    const breedID = e.target.value;
+
+    try {
+        const response = await fetch(baseApiUrl + "images/search?breed_id=" + breedID, apiKey);
+        
+        if (response.ok) {
+            const data = await response.json();
+            showDog(data);
+        } else {
+            throw new Error(`Http error: ${response.status}`);
+        }
+    } catch(error) {
+        console.log(error);
+        alert(`Ups.. it's something wrong. Try again :)`);
+    }  
+}
+
+const showDog = (pictureInfo) => {
+    picture.innerHTML = "";
+    const dogImage = document.createElement('img');
+    dogImage.src = pictureInfo[0].url;
+    dogImage.classList.add('image');
+    const p = document.createElement('p');
+    p.textContent = pictureInfo[0].breeds[0].name;
+    picture.appendChild(dogImage);
+    picture.appendChild(p);
 }
 
 getBreedsNames();
